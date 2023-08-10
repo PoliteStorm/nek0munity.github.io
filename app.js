b6f2c5e23b1f2e3a3a8901d9c1389d04fc0a5fcd
Naturalis <104262669+PoliteStorm@users.noreply.github.com>
2023-08-11 00:47:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let mintCost;
 
 // Replace with the ABI of your contract
 const contractAbi = [
-     {"inputs":[],"stateMutability":"nonpayable","type":"constructor"},
+    {"inputs":[],"stateMutability":"nonpayable","type":"constructor"},
         {"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},
         {"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},
         {"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},
@@ -114,4 +114,3 @@ mintButton.addEventListener('click', async () => {
         console.error('Error minting NFTs:', error);
     }
 });
-
