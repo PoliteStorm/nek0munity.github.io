1b36559d8418ebc816e079e8e10dbb78ef5a383c
Naturalis <104262669+PoliteStorm@users.noreply.github.com>
2023-08-11 03:35:42 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,116 +1,84 @@
-const connectWalletButton = document.getElementById('connectWalletButton');
-const mintButton = document.getElementById('mintButton');
+const web3 = new Web3('<YOUR_INFURA_URL>'); // Replace with your Infura URL or provider
 
-let web3;
-let contract;
-let mintCost;
+const contractAbi = require('./NekoFrensABI.json'); // Replace with your ABI file
+const contractAddress = '0xDf8d126474d3aFd2d8082453540014b503bf0012'; // Replace with your contract address
 
-// Replace with the ABI of your contract
-const contractAbi = [
-    {"inputs":[],"stateMutability":"nonpayable","type":"constructor"},
-        {"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},
-        {"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},
-        {"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},
-        {"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},
-        {"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
-        {"inputs":[],"name":"baseURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},
-        {"inputs":[],"name":"flipMintState","outputs":[],"stateMutability":"nonpayable","type":"function"},
-        {"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},
-        {"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},
-        {"inputs":[],"name":"maxSupply","outputs":[{"internalType":"uint16","name":"","type":"uint16"}],"stateMutability":"view","type":"function"},
-        {"inputs":[{"internalType":"uint256","name":"_mintAmount","type":"uint256"}],"name":"mint","outputs":[],"stateMutability":"payable","type":"function"},
-        {"inputs":[],"name":"mintCost","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
-        {"inputs":[],"name":"mintPaused","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},
-        {"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},
-        {"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},
-        {"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},
-        {"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},
-        {"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},
-        {"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},
-        {"inputs":[{"internalType":"string","name":"_newBaseURI","type":"string"}],"name":"setBaseURI","outputs":[],"stateMutability":"nonpayable","type":"function"},
-        {"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},
-        {"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},
-        {"inputs":[{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
-        {"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenOfOwnerByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
-        {"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},
-        {"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
-        {"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},
-        {"inputs":[],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"}
-];
+const nftContract = new web3.eth.Contract(contractAbi, contractAddress);
 
-// Replace with your actual contract address
-const contractAddress = '0xDf8d126474d3aFd2d8082453540014b503bf0012'; // Paste the contract address here
-
-async function init() {
-    // Initialize web3 and contract instance here
-    if (window.ethereum) {
-        web3 = new Web3(window.ethereum);
-        try {
-            await window.ethereum.enable();
-            contract = new web3.eth.Contract(contractAbi, contractAddress);
-            mintCost = await contract.methods.mintCost().call();
-            mintButton.disabled = false;
-        } catch (error) {
-            console.error('Error initializing web3 or contract:', error);
-        }
+// Function to connect wallet
+async function connectWallet() {
+  if (window.ethereum) {
+    try {
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+      updateWalletInfo();
+      updateMintButtonStatus();
+    } catch (error) {
+      console.error('Error connecting wallet:', error);
     }
+  } else {
+    console.error('No wallet provider available.');
+  }
 }
 
-init();
+// Function to update wallet information
+async function updateWalletInfo() {
+  const accounts = await web3.eth.getAccounts();
+  const balanceWei = await web3.eth.getBalance(accounts[0]);
+  const balanceMatic = web3.utils.fromWei(balanceWei, 'ether');
+  
+  document.getElementById('wallet-address').textContent = accounts[0];
+  document.getElementById('wallet-balance').textContent = balanceMatic;
+}
 
-connectWalletButton.addEventListener('click', async () => {
-    if (!web3) {
-        console.log('Web3 not available.');
-        return;
-    }
+// Function to update mint button status
+function updateMintButtonStatus() {
+  const mintButton = document.getElementById('mint-button');
+  const connectWalletButton = document.getElementById('connect-wallet');
+  
+  const connected = web3.currentProvider.selectedAddress !== null;
+  const balanceMatic = parseFloat(document.getElementById('wallet-balance').textContent);
+  
+  connectWalletButton.disabled = connected;
+  mintButton.disabled = !connected || balanceMatic < 50;
+}
 
-    try {
-        const accounts = await web3.eth.getAccounts();
-        if (accounts.length === 0) {
-            console.log('No connected accounts.');
-            return;
-        }
+// Connect Wallet button click event
+document.getElementById('connect-wallet').addEventListener('click', connectWallet);
 
-        console.log('Connected with address:', accounts[0]);
-    } catch (error) {
-        console.error('Error connecting wallet:', error);
-    }
+// Mint form submit event
+document.getElementById('mint-form').addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const quantity = mintForm.quantity.value;
+  await mintNFTs(quantity);
 });
 
-mintButton.addEventListener('click', async () => {
-    if (!web3 || !contract) {
-        console.log('Web3 or contract instance not available.');
-        return;
-    }
+// Function to mint NFTs
+async function mintNFTs(quantity) {
+  const accounts = await web3.eth.getAccounts();
+  const sender = accounts[0];
 
-    const accounts = await web3.eth.getAccounts();
-    if (accounts.length === 0) {
-        console.log('No connected accounts.');
-        return;
-    }
+  // Estimate gas for the minting transaction
+  const gas = await nftContract.methods.mintNFTs(quantity).estimateGas({ from: sender });
 
-    try {
-        const mintAmount = 1;
+  // Send the minting transaction
+  try {
+    const result = await nftContract.methods.mintNFTs(quantity).send({
+      from: sender,
+      gas,
+    });
 
-        const gasEstimate = await contract.methods.mint(mintAmount).estimateGas({
-            from: accounts[0],
-            value: mintCost
-        });
+    console.log('Transaction hash:', result.transactionHash);
+    console.log('Receipt:', result);
 
-        const gasPrice = await web3.eth.getGasPrice();
-        const gasFee = gasEstimate * gasPrice;
+    // You can provide user feedback that the minting was successful here
+  } catch (error) {
+    console.error('Error minting NFTs:', error);
+    // Provide user feedback that the minting failed
+  }
+}
 
-        const transactionParameters = {
-            from: accounts[0],
-            value: mintCost,
-            gas: gasEstimate,
-            gasPrice: gasPrice
-        };
-
-        await contract.methods.mint(mintAmount).send(transactionParameters);
-
-        console.log('NFTs minted successfully. Gas fee:', web3.utils.fromWei(gasFee.toString(), 'ether'), 'ETH');
-    } catch (error) {
-        console.error('Error minting NFTs:', error);
-    }
+// Update mint button status on page load
+window.addEventListener('load', () => {
+  updateWalletInfo();
+  updateMintButtonStatus();
 });
